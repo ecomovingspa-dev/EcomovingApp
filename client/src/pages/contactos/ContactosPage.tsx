8127bb5bf0df58d9f1f1208ff8613b6c52155046
Mario Osorio <ecomovingspa@gmail.com>
2026-07-21 22:28:31 -0400

    clean: remove prospection campaign warning icon and tooltip from ContactosPage

--- client/src/pages/contactos/ContactosPage.tsx
+++ client/src/pages/contactos/ContactosPage.tsx
@@ -988,16 +988,6 @@ export default function ContactosPage() {
                               }`}
                           />
                         </button>
-                        {contacto.etapa !== "prospeccion" && (contacto.indice_secuencia === -1 || (contacto.indice_secuencia !== null && contacto.indice_secuencia !== undefined && contacto.indice_secuencia > 0)) ? (
-                          <span 
-                            className="text-amber-500 dark:text-amber-400 shrink-0 cursor-help" 
-                            title="Atención: Este contacto ya pasó por la campaña de Prospección anteriormente. Si lo desactivas, se volverá a iniciar la secuencia fría por segunda vez."
-                          >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-3.5 h-3.5">
-                              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                            </svg>
-                          </span>
-                        ) : null}
                       </div>
                     </td>
 
