db41f64a98c477a6013c1f77a3720247eda01a1e
Mario Osorio <ecomovingspa@gmail.com>
2026-07-21 23:37:27 -0400

    feat: show only contacts with emails on ContactosPage

--- client/src/pages/contactos/ContactosPage.tsx
+++ client/src/pages/contactos/ContactosPage.tsx
@@ -302,6 +302,8 @@ export default function ContactosPage() {
         query = query.eq("cuentas.sector", filtroSector);
       }
 
+      // Filtrar para mostrar únicamente contactos que tengan correo electrónico
+      query = query.not("correo", "is", null).neq("correo", "");
 
       const { data, error, count } = await query
         .order("created_at", { ascending: false })
