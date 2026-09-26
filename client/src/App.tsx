251890e68564e32aabb510019467c15f7e70d402
ecomovingspa-dev <ecomovingspa@gmail.com>
2026-09-25 21:42:41 -0300

    Eliminar rutas de Pizarrón en App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,7 +18,6 @@ const ComprasPage = lazy(() => import("./pages/compras/ComprasPage"));
 const ConciliacionPage = lazy(() => import("./pages/conciliacion/ConciliacionPage"));
 const Marketing = lazy(() => import("./pages/marketing/marketing"));
 const PackingPage = lazy(() => import("./pages/logistica/PackingPage"));
-const PizarrONPage = lazy(() => import("./pages/pizarron/PizarrONPage"));
 const SueldoEmpresarialPage = lazy(() => import("./pages/remuneraciones/SueldoEmpresarialPage"));
 
 
@@ -69,9 +68,6 @@ export default function App() {
 
             {/* Marketing */}
             <Route path="marketing" element={<Marketing />} />
-
-            {/* Pizarrón */}
-            <Route path="pizarron" element={<PizarrONPage />} />
 
             {/* Logística - Packing */}
             <Route path="packing" element={<PackingPage />} />
